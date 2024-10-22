feea676991074a9ba03b7b44ba6768e38e9170c2
xiaozeng-zxh <1043644055@qq.com>
2024-10-22 15:23:55 +0800

    si2e code commit

--- SI2E_A2C/rl-starter-files/rl-starter-files/run_sent.sh
+++ SI2E_A2C/rl-starter-files/rl-starter-files/run_sent.sh
@@ -1,7 +1,7 @@
 # A2C + SE
-for task in 'DoorKey-8x8' 'Empty-16x16'
+for task in 'DoorKey-8x8'
 do
-    for seed in 1 10
+    for seed in 1
     do
         python3 -m scripts.train_cuda_0 --algo a2c --env MiniGrid-$task-v0 --model $task/MiniGrid-$task-v0-sent-$seed \
         --save-interval 100 --frames 3000000 --use_entropy_reward --seed $seed --beta 0.005
--- SI2E_A2C/rl-starter-files/rl-starter-files/run_si2e.sh
+++ SI2E_A2C/rl-starter-files/rl-starter-files/run_si2e.sh
@@ -1,7 +1,7 @@
 # A2C + VCSE
-for task in 'DoorKey-8x8' 'Empty-16x16'
+for task in 'DoorKey-8x8'
 do
-    for seed in 1 10 100
+    for seed in 1
     do
         python3 -m scripts.train --algo a2c --env MiniGrid-$task-v0 --model $task/MiniGrid-$task-v0-si2e-$seed \
         --save-interval 100 --frames 3000000 --use_entropy_reward --use_value_condition --seed $seed --beta 0.005 --use_batch
